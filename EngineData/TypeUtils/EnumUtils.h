0f2c0e4dda34fae91139d3a40bf1ee749a76807e
Pavel Grebnev <ruwhynot@gmail.com>
2024-08-31 17:59:11 +0200

    Added a header to be included into autogenerated enum sources

---
+++ EngineData/TypeUtils/EnumUtils.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <vector>
+
+#include "EngineCommon/Types/String/StringId.h"
+
+template<typename T>
+StringId get_enum_name();
+
+template<typename T>
+StringId enum_to_string(T value);
+
+template<typename T>
+T string_to_enum(StringId value);
+
+template<typename T>
+std::vector<T> get_all_enum_values();
+
+template<typename T>
+std::vector<StringId> get_all_enum_value_names();
