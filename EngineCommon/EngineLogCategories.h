2af66b5a6886ff87e9bb2aed237a383e9cc46e18
Pavel Grebnev <ruwhynot@gmail.com>
2025-11-16 09:26:35 +0100

    Added new log category for debug logs

--- EngineCommon/EngineLogCategories.h
+++ EngineCommon/EngineLogCategories.h
@@ -7,3 +7,5 @@ constexpr LogCategory LOG_ASSERT{ "ASSERT" };
 constexpr LogCategory LOG_RENDERING{ "RENDERING" };
 constexpr LogCategory LOG_RESOURCE_LOADING{ "RESOURCE_LOADING" };
 constexpr LogCategory LOG_NETWORK{ "NETWORK" };
+// For debugging only, don't commit these
+constexpr LogCategory LOG_LOCAL_DEBUGGING{ "LOCAL_DEBUGGING" };
